289f0016ddcb0f2ee12978cc4e6c6b9bfb63121d
AllenTom <qweasdzxczse2@gmail.com>
2017-12-08 12:37:09 +0800

    创建amdin控制器

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: TakayamaAren
+ * Date: 12/8/2017
+ * Time: 12:36 PM
+ */
+
+namespace app\admin\controller;
+
+
+use think\Controller;
+
+class Index extends Controller
+{
+
+}
