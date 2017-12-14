339b6bcec4778c0eee0ae20fba3390b2bd4cdae3
AllenTom <qweasdzxczse2@gamil.com>
2017-12-14 15:34:20 +0800

    添加游戏

--- application/admin/controller/AdminModelController.php
+++ application/admin/controller/AdminModelController.php
@@ -11,17 +11,46 @@ namespace app\admin\controller;
 
 use think\Model;
 
+/**
+ * 模型类，快速建立Model
+ * Class AdminModelController
+ * @package app\admin\controller
+ */
 abstract class AdminModelController extends BaseAdminController
 {
+    /**
+     * @var string[] 保存时获取的字段
+     */
+    private $saveField;
+    /**
+     * @var string 操作成功时重定向位置
+     */
+    private $actionRedirect;
+
     /**
      * 获取Model
      * @return Model 模型
      */
     public abstract function getModel();
+
     public function index()
     {
         $this->assign("modelList", $this->getModel()->select());
         return parent::index();
+    }
+
+    /**
+     * 创建Model使用
+     */
+    public function save()
+    {
+        $data = [];
+        foreach ($this->saveField as $field) {
+            array_push($data, $this->request->post($field));
+        }
+        $model = $this->getModel();
+        $model->save($data);
+        $this->redirect($this->actionRedirect);
     }
 
 
